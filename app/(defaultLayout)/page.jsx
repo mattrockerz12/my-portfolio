89a02f8d21ac80ec03e0240684c0b1964f29ee41
John Matthew <mattrockerz35@gmail.com>
2026-09-17 16:39:37 +0800

    About and Resume content added

--- app/(defaultLayout)/page.jsx
+++ app/(defaultLayout)/page.jsx
@@ -2,6 +2,10 @@
 import data from "../Data.json";
 import React from "react";
 import Hero from "../ui/Hero/Hero";
+import About from "../ui/About/About";
+import Iconbox from "../ui/Iconbox/Iconbox";
+import Skill from "../ui/Skill/Skill";
+import Resume from "../ui/Resume/ResumeSection";
 
 export default function page() {
   const {
@@ -20,6 +24,10 @@ export default function page() {
     <>
       <div className="st-height-b80 st-height-lg-b80"></div>
       <Hero data={heroData.homeOneHero} socialData={socialData} />
+      <About data={aboutData} data-aos="fade-right" />
+      <Iconbox data={serviceData} data-aos="fade-right" />
+      <Skill data={skillData} data-aos="fade-right" />
+      <Resume data={resumeData} />
     </>
   );
 }
